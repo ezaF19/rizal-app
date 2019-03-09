585fa62ca38b39f087de5a64dde629ef67bb1a46
kyleaquino <kshaunaquino@gmail.com>
2019-03-09 23:42:53 +0800

    update settings

--- rizal/settings.py
+++ rizal/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'alcuhhb1$irs&!vkb-g7w@io$xc8c1@p4czhe4@np80k3=kurb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
